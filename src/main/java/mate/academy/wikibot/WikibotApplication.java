2f8a67350686b3a667185aadbd2f289d07c1ad46
chupika.bohdan@ukr.net <Zamanalumene5>
2019-02-26 20:18:45 +0200

    added startup class

---
+++ src/main/java/mate/academy/wikibot/WikibotApplication.java
@@ -0,0 +1,13 @@
+package mate.academy.wikibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikibotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikibotApplication.class, args);
+    }
+
+}
