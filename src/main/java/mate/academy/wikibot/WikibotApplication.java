55a07ebd6a985a8dfa20d294eb425a23466ebd05
Anatolii <dinexpod@gmail.com>
2019-03-03 18:42:02 +0200

    - create method "doReques" fot receiving response code.

--- src/main/java/mate/academy/wikibot/WikibotApplication.java
+++ src/main/java/mate/academy/wikibot/WikibotApplication.java
@@ -1,5 +1,6 @@
 package mate.academy.wikibot;
 
+import mate.academy.wikibot.controllers.RequestController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +9,9 @@ public class WikibotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WikibotApplication.class, args);
+
+        String query = "http://www.google.com.ua";
+
+        System.out.println(RequestController.doRequest(query, "GET"));
     }
 }
---
+++ src/main/java/mate/academy/wikibot/controllers/RequestController.java
@@ -0,0 +1,40 @@
+package mate.academy.wikibot.controllers;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class RequestController {
+
+    public static String doRequest(String link, String requestMethod) {
+
+        HttpURLConnection connection = null;
+        String responseMessage = null;
+
+        try {
+            connection = (HttpURLConnection) new URL(link).openConnection();
+
+            connection.setRequestMethod(requestMethod);
+            connection.setUseCaches(false);
+            connection.setConnectTimeout(250);
+            connection.setReadTimeout(250);
+
+            connection.connect();
+
+            if (HttpURLConnection.HTTP_OK == connection.getResponseCode()) {
+                responseMessage = "Status: " + connection.getResponseCode() + ". Congradulation!!!";
+            } else {
+                responseMessage = "Failed. RECEIVE RESPONSE CODE:" + connection.getResponseCode()
+                        + ". RESPONSE MESSAGE: " + connection.getResponseMessage();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+
+        return responseMessage;
+    }
+}
