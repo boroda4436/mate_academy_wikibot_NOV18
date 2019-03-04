a812384bc4808a077ee442b829e3d38e272f5a9d
romaanankin <romaanankin@gmail.com>
2019-03-04 13:18:13 +0200

    RequestDto refactored

--- src/main/java/mate/academy/wikibot/dto/RequestDto.java
+++
@@ -1,6 +0,0 @@
-package mate.academy.wikibot.dto;
-
-public class RequestDto {
-    private String chat_id;
-    private String text;
-}
---
+++ src/main/java/mate/academy/wikibot/dto/ResponseDto.java
@@ -0,0 +1,10 @@
+package mate.academy.wikibot.dto;
+
+public class ResponseDto {
+    private String kind;
+    private String videoId;
+    private String title;
+    private String url;
+}
+
+
