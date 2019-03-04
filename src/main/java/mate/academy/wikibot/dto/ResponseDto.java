25d6efcd7267e92031119cb7f4901da8f041a7f7
romaanankin <romaanankin@gmail.com>
2019-03-04 14:49:16 +0200

    refactored

--- src/main/java/mate/academy/wikibot/dto/ResponseDto.java
+++ src/main/java/mate/academy/wikibot/dto/ResponseDto.java
@@ -1,10 +1,11 @@
 package mate.academy.wikibot.dto;
 
+import lombok.Data;
+
+@Data
 public class ResponseDto {
     private String kind;
     private String videoId;
     private String title;
     private String url;
 }
-
-
