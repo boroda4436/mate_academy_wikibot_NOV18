e360dda828da9b5e23c86971c3117ff89abbce36
Illia1927 <edcwsxqaz22>
2019-03-05 19:25:41 +0200

    do the telegram bot

---
+++ src/main/java/mate/academy/bot/Bot.java
@@ -0,0 +1,53 @@
+package mate.academy.bot;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+    public static void main(String[] args) {
+
+        ApiContextInitializer.init(); // Инициализируем апи
+        TelegramBotsApi botapi = new TelegramBotsApi();
+        try {
+            botapi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        String message = update.getMessage().getText();
+        sendMsg(update.getMessage().getChatId().toString(), message);
+    }
+
+    public synchronized void sendMsg(String chatId, String s) {
+        if (s.equals("/start")) {
+            s = "Hello World";
+        }
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "wiki_mate_academy_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "536065274:AAEL-fRfzbXB5EM-FNsDutBLYvSDhQHWx10";
+    }
+
+}
