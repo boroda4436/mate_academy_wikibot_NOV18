7451d6b0579e210941b44d2fc1b8a4fc47a510d4
romaanankin <romaanankin@gmail.com>
2019-03-09 15:08:43 +0200

    refactored

--- src/main/java/mate/academy/wikibot/dto/ResponseConverter.java
+++ src/main/java/mate/academy/wikibot/dto/ResponseConverter.java
@@ -2,13 +2,17 @@ package mate.academy.wikibot.dto;
 
 import lombok.Data;
 
+/**
+ * Class performs convertion from ResponseDto to MessageEntityDto.
+ */
 @Data
 public class ResponseConverter {
+
     public MessageEntityDto convertToMessage(ResponseDto responseDto) {
-       MessageEntityDto messageEntityDto = new MessageEntityDto();
-       messageEntityDto.setUrl(responseDto.getVideoId());
-       messageEntityDto.setType("url");
-       messageEntityDto.setLength(responseDto.getVideoId().length());
-       return messageEntityDto;
+        MessageEntityDto messageEntityDto = new MessageEntityDto();
+        messageEntityDto.setUrl(responseDto.getVideoId());
+        messageEntityDto.setType("url");
+        messageEntityDto.setLength(responseDto.getVideoId().length());
+        return messageEntityDto;
     }
 }
