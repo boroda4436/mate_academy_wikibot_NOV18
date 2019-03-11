0a49739bc98f8e081dc3e87ce372690d7ca96bd7
dmitriy <dimalitvin192.0@gmail.com>
2019-03-11 11:11:28 +0200

    added style-code changes in YouTubeRequestController

--- src/main/java/mate/academy/wikibot/http/YouTubeRequestController.java
+++ src/main/java/mate/academy/wikibot/http/YouTubeRequestController.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-GI
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
