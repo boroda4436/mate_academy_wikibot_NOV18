756b6d7b712a59bf324af4da5ae92caba26b1c38
romaanankin <romaanankin@gmail.com>
2019-03-14 19:37:36 +0200

    refactored

--- src/main/java/mate/academy/wikibot/WikibotApplication.java
+++ src/main/java/mate/academy/wikibot/WikibotApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WikibotApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable {
         SpringApplication.run(WikibotApplication.class, args);
     }
 }
