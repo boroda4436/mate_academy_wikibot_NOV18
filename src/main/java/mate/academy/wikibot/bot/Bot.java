22a9da524c32ae468935d59e1342422c5a027ab3
Orbit419 <rkrylov97@gmail.com>
2019-03-19 09:23:31 +0200

    Refactored v2

--- src/main/java/mate/academy/wikibot/bot/Bot.java
+++ src/main/java/mate/academy/wikibot/bot/Bot.java
@@ -1,8 +1,8 @@
 package mate.academy.wikibot.bot;
 
 import com.google.api.client.util.DateTime;
-import mate.academy.wikibot.logs.LogRepository;
-import mate.academy.wikibot.logs.Record;
+import mate.academy.wikibot.logs.LogRecordRepository;
+import mate.academy.wikibot.logs.LogRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -18,7 +18,7 @@ public class Bot extends TelegramLongPollingBot {
     @Value("${bot.token}")
     private String botToken;
     @Autowired
-    private LogRepository logRepository;
+    private LogRecordRepository logRecordRepository;
 
     @Override
     public void onUpdateReceived(Update update) {
@@ -53,12 +53,12 @@ public class Bot extends TelegramLongPollingBot {
     }
 
     private void log(Update update) {
-        Record record = Record.builder()
+        LogRecord logRecord = LogRecord.builder()
                 .message(update.getMessage().getText())
                 .chatId(update.getMessage().getChatId())
                 .date(new DateTime(update.getMessage().getDate() * 1000L))
                 .build();
 
-        logRepository.save(record);
+        logRecordRepository.save(logRecord);
     }
 }
--- src/main/java/mate/academy/wikibot/logs/LogRecordRepository.java
+++ src/main/java/mate/academy/wikibot/logs/LogRecordRepository.java
@@ -2,5 +2,5 @@ package mate.academy.wikibot.logs;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface LogRepository extends CrudRepository<Record, Long> {
+public interface LogRecordRepository extends CrudRepository<LogRecord, Long> {
 }
