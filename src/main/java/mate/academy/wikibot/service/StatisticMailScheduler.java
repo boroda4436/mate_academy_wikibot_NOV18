a5625b2afdbb9dbb74709b44e8261dfa8404a2a7
boroda4436 <chupika.bohdan@ukr.net>
2019-03-24 14:54:44 +0200

    MAWB-21: refactored HttpClient

--- src/main/java/mate/academy/wikibot/service/StatisticMailScheduler.java
+++ src/main/java/mate/academy/wikibot/service/StatisticMailScheduler.java
@@ -1,28 +1,28 @@
 package mate.academy.wikibot.service;
 
 import mate.academy.wikibot.dto.SendMailRequest;
-import mate.academy.wikibot.http.HttpClient;
+import mate.academy.wikibot.dto.SendMailResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+
 @Component
-public class MailScheduler {
-    private final HttpClient httpClient;
+public class StatisticMailScheduler {
+    private final MailService mailService;
 
     @Autowired
-    public MailScheduler(HttpClient httpClient) {
-        this.httpClient = httpClient;
+    public StatisticMailScheduler(MailService mailService) {
+        this.mailService = mailService;
     }
 
     /**
      * Send message. Please IMPLEMENT: add scheduler every week
      */
-    public void sendMessage() {
+    public SendMailResponse sendMessage() throws Exception {
         SendMailRequest sendMailRequest = new SendMailRequest();
         sendMailRequest.setSendTo("<add email here>");
         sendMailRequest.setTitle("Bot statistic");
         sendMailRequest.setMessage("Hello world");
-
-        httpClient.doPost("http://localhost:8081/mail/send", sendMailRequest);
+        return mailService.sendMessage(sendMailRequest);
     }
 }
