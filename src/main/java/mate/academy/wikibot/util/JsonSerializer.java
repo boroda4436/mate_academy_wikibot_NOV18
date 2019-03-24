8f6a8f8627ff47dde272796a8db882e65a3f9ed0
boroda4436 <chupika.bohdan@ukr.net>
2019-03-24 15:14:47 +0200

    MAWB-21: refactored JsonSerializer: remove unnecessary methods

--- src/main/java/mate/academy/wikibot/util/JsonSerializer.java
+++ src/main/java/mate/academy/wikibot/util/JsonSerializer.java
@@ -3,10 +3,8 @@ package mate.academy.wikibot.util;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 
 import org.springframework.stereotype.Component;
 
@@ -16,20 +14,6 @@ public class JsonSerializer implements BaseSerializer {
 
     public JsonSerializer() {
         jsonSerializer.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    }
-
-    /**
-     * Deserializes specified object from buffered reader.
-     *
-     * @param clazz objects class type to deserialize.
-     * @param in buffered input reader.
-     *
-     * @return deserialized object.
-     *
-     * @throws Exception when IO error occurs.
-     */
-    public <T> T deserialize(Class<T> clazz, BufferedReader in) throws Exception {
-        return jsonSerializer.readValue(in, clazz);
     }
 
     /**
@@ -44,17 +28,6 @@ public class JsonSerializer implements BaseSerializer {
      */
     public <T> T deserialize(Class<T> clazz, InputStream in) throws Exception {
         return jsonSerializer.readValue(in, clazz);
-    }
-
-    /**
-     * Serializes the specified object into print writer in JSON format.
-     *
-     * @param serialiedObj object to serialize
-     * @param out print writer to serialize to.
-     * @throws Exception when IO error occurs.
-     */
-    public void serialize(Object serialiedObj, PrintWriter out) throws Exception {
-        jsonSerializer.writeValue(out, serialiedObj);
     }
 
     /**
