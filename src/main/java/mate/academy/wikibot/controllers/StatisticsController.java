64a10d502820167f2c921da23c47fec036aca29a
boroda4436 <chupika.bohdan@ukr.net>
2019-07-12 15:34:33 +0300

    refactoring

--- src/main/java/mate/academy/wikibot/controllers/StatisticsController.java
+++ src/main/java/mate/academy/wikibot/controllers/StatisticsController.java
@@ -13,10 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "statistic")
+@RestController
+@RequestMapping(value = "statistic")
 public class StatisticsController {
     private final LogRecordRepository recordRepository;
 
